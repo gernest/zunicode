aef82b5072173c690225cc1e91d54baff206200f
gernest <geofreyernest@live.com>
2018-11-04 09:23:35 +0300

    add test

--- src/utf8/index_test.zig
+++ src/utf8/index_test.zig
@@ -6,4 +6,7 @@ test "init" {
     if (utf8.max_rune != unicode.base.max_rune) {
         try t.terror("wrong utf8.max_rune");
     }
+    if (utf8.rune_error != unicode.base.replacement_char) {
+        try t.terror("wrong utf8.rune_error");
+    }
 }
