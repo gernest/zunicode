714a94bcf7f9d659e25728b5d9cce184016b67a0
gernest <geofreyernest@live.com>
2019-10-30 09:18:46 +0300

    fix test

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    var main_tests = b.addTest("src/all_test.zig");
+    var main_tests = b.addTest("src/unicode_test.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
